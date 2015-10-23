665114f6616092d988dcebb24220c120f154f692
instantblog <instantblog@hotmail.com>
2015-10-23 13:11:25 -0600

    refine

--- src/app/components/blog-timeline/blog-timeline.directive.js
+++ src/app/components/blog-timeline/blog-timeline.directive.js
@@ -1,14 +1,26 @@
 'use strict';
 
 angular.module('instantblog')
-  .directive('blogTimeline', function () {
+  .directive('blogTimeline', function ($window, $log) {
     return {
       templateUrl: 'app/components/blog-timeline/blog-timeline.html',
-      scope:{
-        'galleries':'=galleries'
+      scope: {
+        'galleries': '=galleries'
       },
       restrict: 'EA',
-      link: function () {
+      link: function (scope,element) {
+        var window = angular.element($window);
+        var width = $window.innerWidth;
+
+        scope.isSmallScreen = width < 600;
+
+        window.on("resize", function () {
+          $log.log($window.innerWidth);
+          scope.isSmallScreen = width < 600;
+        });
+        scope.$on("$destroy", function () {
+          window.off("resize");
+        });
       }
     };
   });
